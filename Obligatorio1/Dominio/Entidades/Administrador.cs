05c5158b4d74560b7000a902ef796f3331c1997a
maurivarela02 <mauricio.varela@2innovateit.com>
2024-09-10 21:02:30 -0300

    Se agrega clase Usuario, Cliente y Administrador

---
+++ Obligatorio1/Dominio/Entidades/Administrador.cs
@@ -0,0 +1,25 @@
+﻿namespace Dominio.Entidades
+{
+    public class Administrador : Usuario
+    {
+        public bool Admin { get; }
+
+        public Administrador(string nombre, string apellido, string email, string contrasenia) :
+            base(nombre, apellido, email, contrasenia)
+        {
+            Admin = true;
+        }
+
+
+        public override string ToString()
+        {
+            string respuesta = string.Empty;
+
+            respuesta += $"Nombre: {Nombre} \n";
+            respuesta += $"Apellido: {Apellido} \n";
+            respuesta += $"Email: {Email} \n";
+            if(Admin)respuesta += $"Soy administrador";
+            return respuesta;
+        }
+    }
+}
---
+++ Obligatorio1/Dominio/Entidades/Usuario.cs
@@ -0,0 +1,37 @@
+﻿namespace Dominio.Entidades
+{
+    public class Usuario
+    {
+
+        private static int _ultimoId;
+
+        public int Id { get; }
+        public string Nombre { get; }
+        public string Apellido { get; }
+        public string Email { get; }
+        public string Contrasenia { get; }
+        public Usuario(string nombre, string apellido, string email, string contrasenia)
+        {
+            Id = _ultimoId++;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Contrasenia  = contrasenia;
+        }
+
+        public void Validar()
+        { 
+        
+        }
+
+        public override string ToString()
+        {
+            string respuesta = string.Empty;
+
+            respuesta += $"Nombre: {Nombre} \n";
+            respuesta += $"Apellido: {Apellido} \n";
+            respuesta += $"Email: {Email} \n";
+            return respuesta;
+        }
+    }
+}
