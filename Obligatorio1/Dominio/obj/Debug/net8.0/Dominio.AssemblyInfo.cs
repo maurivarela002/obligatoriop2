e476975e59ea96bba958b8d16ae7207aa2dc5c37
Matialv15 <matialv15@gmail.com>
2024-09-21 12:49:21 -0300

    Carga inicial rama mati

--- Obligatorio1/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Obligatorio1/Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534bfa639980b7510401f06f30b5bb15cb26cf7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2bf3cef0f4f1ab9444f6bd256458dd1388a728")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
