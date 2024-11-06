0c1471c273d559f97ad167e1cce9fd7020859a83
matialv15 <matialv15@gmail.com>
2024-11-05 21:39:36 -0300

    ventas relampagos, ultima oferta de subasta el boton que muestra si el estado esta abierta

--- Obligatorio1/Dominio/Entidades/Venta.cs
+++ Obligatorio1/Dominio/Entidades/Venta.cs
@@ -2,62 +2,76 @@
 
 
 using Dominio.Interfaces;
+using System.Reflection.Metadata.Ecma335;
 using static Dominio.Sistema;
 
 namespace Dominio.Entidades
 {
-    public class Venta : Publicacion
-    {
-        public bool OfertaR { get; set; }
-        public object Articulos { get; private set; }
-        public List<Articulo> ObtenerArtxPub { get; set; }
-        public Venta(
-                     string nombre,
-                     EnumEstados estados,
-                     DateTime fchPublic,
-                     //List<Articulo> articulos,
-                     int idUser,
-                     int idPurchUser,
-                     DateTime purchDate,
-                     bool ofertar
-            ) : base(nombre, estados, fchPublic, 
-                //articulos, 
-                idUser, idPurchUser, purchDate)
-        {
-            OfertaR = ofertar;
+	public class Venta : Publicacion
+	{
+		public bool OfertaR { get; set; }
+		public object Articulos { get; private set; }
+		public List<Articulo> ObtenerArtxPub { get; set; }
+		public Venta(
+					 string nombre,
+					 EnumEstados estados,
+					 DateTime fchPublic,
+					 //List<Articulo> articulos,
+					 int idUser,
+					 int idPurchUser,
+					 DateTime purchDate,
+					 bool ofertar
+			) : base(nombre, estados, fchPublic,
+				//articulos, 
+				idUser, idPurchUser, purchDate)
+		{
+			OfertaR = ofertar;
 
-        }
-        public virtual void Validar()
-        {
-            validateNull();
-        }
-        private void validateNull()
-        {
-            if (string.IsNullOrEmpty(base.Nombre))
-            {
-                throw new Exception("El nombre no puede ser vacio");
-            }
-        }
+		}
+		public virtual void Validar()
+		{
+			validateNull();
+		}
+		private void validateNull()
+		{
+			if (string.IsNullOrEmpty(base.Nombre))
+			{
+				throw new Exception("El nombre no puede ser vacio");
+			}
+		}
 
-        public override string Tipo()
-        {
-            string salida = "Venta";
-            return salida;
-        }
+		public override string Tipo()
+		{
+			string salida = "Venta";
+			return salida;
+		}
 
-        public override string BtnComprar()
-        {
-            string salida = "Comprar";
-            return salida;
-        }
+		public override string BtnComprar()
+		{
+			string salida = "Comprar";
+			return salida;
+		}
 		public override string ToString()
-        {
-            string respuesta = base.ToString();
-            if (OfertaR)
-            {
-                respuesta += $"Oferta Relampago \n";
-            }
-            return respuesta;
-        }
-    }
+		{
+			string respuesta = base.ToString();
+			if (OfertaR)
+			{
+				respuesta += $"Oferta Relampago \n";
+			}
+			return respuesta;
+		}
+
+		public override double PrecioPublicacion()
+		{
+			double total = base.PrecioPublicacion();
+
+			if (OfertaR) 
+			{
+				total = Math.Round(total * 0.8);
+			}
+			return total;
+		}
+
+
+	}
 }
